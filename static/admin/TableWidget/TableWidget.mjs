af84a2ebd81d3e8163a991f848f1440f407f6acc
Siddhant Agarwal <siddhant.agarwal@razorpay.com>
2021-04-07 15:48:07 +0530

    Added custom widget

--- static/admin/TableWidget/TableWidget.mjs
+++ static/admin/TableWidget/TableWidget.mjs
@@ -1,9 +1,27 @@
 import React from "react";
 
-export function TableWidgetPreivew() {
-  return <div>Table Widget</div>;
+export function TableWidgetPreivew(props) {
+  const { value } = props;
+  console.log(value);
+  return <div>{JSON.stringify(value)}</div>;
 }
 
-export function TableWidgetControl() {
-  return <div>Table Widget</div>;
+export function TableWidgetControl(props) {
+  const { value, onChange } = props;
+
+  function handleOnChange(event) {
+    onChange(event.target.value);
+  }
+
+  return (
+    <div
+      style={{
+        border: "2px solid lightgrey",
+        padding: "16px",
+        borderRadius: "4px",
+      }}
+    >
+      <input value={value} onChange={handleOnChange} />
+    </div>
+  );
 }
