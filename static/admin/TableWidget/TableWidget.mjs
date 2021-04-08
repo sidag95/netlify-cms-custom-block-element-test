e24cdb9e476f337a531cfedda7f41c6bb19e8d2a
Siddhant Agarwal <siddhant.agarwal@razorpay.com>
2021-04-08 10:34:23 +0530

    Refactor

--- static/admin/TableWidget/TableWidget.mjs
+++ static/admin/TableWidget/TableWidget.mjs
@@ -35,3 +35,29 @@ export function TableWidgetControl(props) {
     </table>
   );
 }
+
+export const tableWidgetDefinition = {
+  // Internal id of the component
+  id: "table",
+  // Visible label
+  label: "Table",
+  // Fields the user need to fill out when adding an instance of the component
+  fields: [{ name: "tableName", label: "Table", widget: "table" }],
+  // Pattern to identify a block as being an instance of this component
+  pattern: /^<table>([\s\S]*?)<\/table>/,
+  // Function to extract data elements from the regexp match
+  fromBlock: function (match) {
+    return {
+      data: match[1],
+    };
+  },
+  // Function to create a text block from an instance of this component
+  toBlock: function (obj) {
+    return `<table>${obj.tableName}</table>`;
+  },
+  // Preview output for this component. Can either be a string or a React component
+  // (component gives better render performance)
+  toPreview: function (obj) {
+    return <TableWidgetPreivew value={obj.data} />;
+  },
+};
--- static/admin/index.mjs
+++ static/admin/index.mjs
@@ -3,38 +3,8 @@ import CMS from "netlify-cms-app";
 import {
   TableWidgetControl,
   TableWidgetPreivew,
+  tableWidgetDefinition,
 } from "./TableWidget/TableWidget.mjs";
-
-export default function PagePreview(props) {
-  console.log(props.entry.getIn(["data"]).toJS());
-  return <div className="preview">Preview</div>;
-}
-
-const tableWidgetDefinition = {
-  // Internal id of the component
-  id: "table",
-  // Visible label
-  label: "Table",
-  // Fields the user need to fill out when adding an instance of the component
-  fields: [{ name: "tableName", label: "Table", widget: "table" }],
-  // Pattern to identify a block as being an instance of this component
-  pattern: /^<table>([\s\S]*?)<\/table>/,
-  // Function to extract data elements from the regexp match
-  fromBlock: function (match) {
-    return {
-      data: match[1],
-    };
-  },
-  // Function to create a text block from an instance of this component
-  toBlock: function (obj) {
-    return `<table>${obj.tableName}</table>`;
-  },
-  // Preview output for this component. Can either be a string or a React component
-  // (component gives better render performance)
-  toPreview: function (obj) {
-    return <TableWidgetPreivew value={obj.data} />;
-  },
-};
 
 // CMS.registerPreviewTemplate("post", PagePreview);
 CMS.registerWidget("table", TableWidgetControl, TableWidgetPreivew);
